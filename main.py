67d4c6b8a4109b11fa9330d672382b27d2422ecf
Nyako <rachel.rot@bk.ru>
2024-11-29 12:18:58 +0300

    Revert "Update main.py"
    This reverts commit 26d5bad2467d475b18ac99c46f8709d5cf01a290.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from tkinter import *
 root = Tk()     # создаем корневой объект - окно
 root.title("Calculator")     # устанавливаем заголовок окна
-root.geometry("500x650")    # устанавливаем размеры окна
+root.geometry("300x250")    # устанавливаем размеры окна
  
 root.mainloop()
