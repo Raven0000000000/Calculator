e03467fb7e1c5abb29cbf6fc44476c75cce4aabb
Nyako <rachel.rot@bk.ru>
2024-11-29 21:13:25 +0300

    Update main.py
    создание лэйбла для вывода в дальнейшем информации при взаимодействии с кнопкамии

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from tkinter import *
-root = Tk()     # создаем корневой объект - окно
-root.title("Calculator")     # устанавливаем заголовок окна
-root.geometry("500x250")    # устанавливаем размеры окна
+windows = Tk()     # создаем корневой объект - окно
+windows.title("Calculator")     # устанавливаем заголовок окна
+windows.geometry("500x650")    # устанавливаем размеры окна
 
+label_out = Label(
+    text = "0",
+    font=("Arial", 44),
+    fg="black",
+    bg="snow3"
+)
+label_out.pack(anchor=N, padx=5,pady=5, side=RIGHT)
 
-# new comments
-
-# another comment
-root.mainloop()
+windows.mainloop()
