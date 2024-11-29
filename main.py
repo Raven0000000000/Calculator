c25cc25fbf0e4abd322bd76e7ee1b0fcf3586832
Nyako <rachel.rot@bk.ru>
2024-11-29 20:01:55 +0300

    1

--- main.py
+++ main.py
@@ -2,5 +2,8 @@ from tkinter import *
 root = Tk()     # создаем корневой объект - окно
 root.title("Calculator")     # устанавливаем заголовок окна
 root.geometry("500x250")    # устанавливаем размеры окна
- 
+
+
+# new comments
+
 root.mainloop()
